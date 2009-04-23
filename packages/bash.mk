5c97eeee18840a65c1a87ba31ac13d9288ec9b8d
Eliot Dresselhaus <eliot@localhost.localdomain>
2009-04-23 14:36:16 -0700

    make || fails

--- packages/bash.mk
+++ packages/bash.mk
@@ -7,6 +7,8 @@ bash_configure_args += --with-installed-readline=$(INSTALL_DIR)/readline
 # If you need a static bash uncomment the following line
 #bash_configure_args += --enable-static-link
 
+bash_make_parallel_fails = yes
+
 bash_CPPFLAGS = $(call installed_includes_fn, ncurses readline)
 bash_LDFLAGS = $(call installed_libs_fn, ncurses readline)
 
