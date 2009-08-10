868a15927b893b724cc9c40a541889cbaccf1fe6
Timothy Mattox <timattox@bxb-lds-082.(none)>
2009-08-10 16:55:26 -0400

    Use $(TARGET) in the ompi.mk so that the proper crosscompiler is selected at configure time.

--- packages/ompi.mk
+++ packages/ompi.mk
@@ -1,3 +1,3 @@
 ompi_configure_args += \
 	--with-platform=../../../ompi/contrib/platform/cisco/hlfr/ebuild \
-        --build=i386-unknown-linux-gnu --target=ppc-q-linux
+        --build=i386-unknown-linux-gnu --host=$(TARGET)
