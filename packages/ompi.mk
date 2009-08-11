e29cc2522f3634f461d93cfe9c6647de40f6d74e
Timothy Mattox <timattox@bxb-lds-082.(none)>
2009-08-11 12:04:06 -0400

    Add in target specific configure args to ompi.mk, along with common case args. Note: The ebuild system adds --host=blah to the configure args automatically, so those args were removed from the ompi.mk so they aren't duplicated.

--- packages/ompi.mk
+++ packages/ompi.mk
@@ -1,9 +1,17 @@
 ompi_platform_dir = $(call find_source_fn,$(PACKAGE_SOURCE))/contrib/platform
 
 # target dependent configure args
+# PLATFORM=qasmp
 ompi_configure_args_ppc-q-linux = \
-  --build=i386-unknown-linux-gnu \
-  --with-platform=$(ompi_platform_dir)/cisco/hlfr/ebuild \
-  --host=$(TARGET)
+  --build=i386-unknown-linux-gnu
 
-ompi_configure_args = $(ompi_configure_args_$(TARGET))
+# PLATFORM=qsp
+ompi_configure_args_ppc-qsp-linux = \
+  --build=i386-unknown-linux-gnu
+
+# PLATFORM=native, NOTE: $(TARGET) is the empty string
+ompi_configure_args_ = 
+
+# combine target specific args with general configure args
+ompi_configure_args = $(ompi_configure_args_$(TARGET)) \
+  --with-platform=$(ompi_platform_dir)/cisco/hlfr/ebuild
