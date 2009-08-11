53ea296779e1c30a7a0462074b69613d6e5ae605
David Barach <dbarach@sw-oplin-1.cisco.com>
2009-08-11 07:22:13 -0400

    Cleanup.

--- packages/ompi.mk
+++ packages/ompi.mk
@@ -1,3 +1,9 @@
-ompi_configure_args += \
-	--with-platform=../../../ompi/contrib/platform/cisco/hlfr/ebuild \
-        --build=i386-unknown-linux-gnu --host=$(TARGET)
+ompi_platform_dir = $(call find_source_fn,$(PACKAGE_SOURCE))/contrib/platform
+
+# target dependent configure args
+ompi_configure_args_ppc-q-linux = \
+  --build=i386-unknown-linux-gnu \
+  --with-platform=$(ompi_platform_dir)/cisco/hlfr/ebuild \
+  --host=$(TARGET)
+
+ompi_configure_args = $(ompi_configure_args_$(TARGET))
