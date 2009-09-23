440df2035486f43331f5dc109487b31a84778ada
N Srinivasan <nsriniva@sjc-lds-304.(none)>
2009-09-23 14:05:47 -0700

    Modified the libnetfilter_queue.mk file to correctly reflect the package name.

--- packages/libnetfilter_queue.mk
+++ packages/libnetfilter_queue.mk
@@ -1,3 +1,3 @@
-libnetfilter-queue_configure_depend = libnfnetlink-install
-libnetfilter-queue_CFLAGS = $(call installed_includes_fn, libnfnetlink)
-libnetfilter-queue_LDFLAGS = $(call installed_libs_fn, libnfnetlink)
+libnetfilter_queue_configure_depend = libnfnetlink-install
+libnetfilter_queue_CFLAGS = $(call installed_includes_fn, libnfnetlink)
+libnetfilter_queue_LDFLAGS = $(call installed_libs_fn, libnfnetlink)
