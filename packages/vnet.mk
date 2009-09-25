8ad5a09d59033a40af36791299f4a515c1b5d485
David Barach <dbarach@ludd-79.cisco.com>
2009-09-25 14:04:56 -0700

    Shared-memory and socket API interfaces.

--- packages/vnet.mk
+++ packages/vnet.mk
@@ -1,4 +1,4 @@
-vnet_configure_depend = vlib-install clib-install
+vnet_configure_depend = vlib-install clib-install svm-install
 
-vnet_CPPFLAGS = $(call installed_includes_fn, vlib clib)
-vnet_LDFLAGS = $(call installed_libs_fn, vlib clib)
+vnet_CPPFLAGS = $(call installed_includes_fn, vlib clib svm)
+vnet_LDFLAGS = $(call installed_libs_fn, vlib clib svm)
