5f275c867283f80b404e9d9dd3904a6118964053
Burt Silverman <bsilverm@sjc-lds-035.(none)>
2009-10-08 09:47:03 -0700

    Need to add "build" directory to $(PACKAGE_BUILD_DIR) and we now pickup header files from $(INSTALL_DIR)/sysfsutils/include.

--- packages/pcmciautils.mk
+++ packages/pcmciautils.mk
@@ -5,14 +5,15 @@ top_srcdir = $(call find_source_fn,pcmciautils)
 pcmciautils_configure = \
   cp $(top_srcdir)/Makefile $(PACKAGE_BUILD_DIR); \
   mkdir -p $(PACKAGE_BUILD_DIR)/src		;\
-  mkdir -p $(PACKAGE_BUILD_DIR)/udev
+  mkdir -p $(PACKAGE_BUILD_DIR)/udev		;\
+  mkdir -p $(PACKAGE_BUILD_DIR)/build
 
 pcmciautils_make_args = CROSS=$(TARGET)-
 pcmciautils_make_args += prefix=$(INSTALL_DIR)/pcmciautils
 pcmciautils_make_args += top_srcdir=$(call find_source_fn,pcmciautils)
 
 pcmciautils_build = \
-  export CPPFLAGS="-I$(TOOL_INSTALL_DIR)/usr/include -I$(top_srcdir)/src";\
+  export CPPFLAGS="-I$(INSTALL_DIR)/sysfsutils/include -I$(top_srcdir)/src";\
   export LDFLAGS="$(call installed_libs_fn, sysfsutils)"		 ;\
   make $(pcmciautils_make_args)						 ;\
   unset CPPFLAGS LDFLAGS
