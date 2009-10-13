e600a579ef2f34af8a20fc0e0472770abd843971
Burt Silverman <bsilverm@sjc-lds-035.(none)>
2009-10-12 18:15:26 -0700

    Add a valgrind_image_include function that puts all valgrind's /usr/bin, /lib/valgrind and /lib/pkgconfig stuff in the image, except for .a files.

--- packages/valgrind.mk
+++ packages/valgrind.mk
@@ -3,3 +3,13 @@ valgrind_CCASFLAGS += -I..
 valgrind_configure_args = --prefix=/usr --libdir=/lib
 
 valgrind_install_args = DESTDIR=$(PACKAGE_INSTALL_DIR)
+
+# Reports shared libraries (and more) in given directory
+valgrind_find_shared_libs_fn =  find $(1) -mindepth 2 -maxdepth 2 \
+				! -regex $(arch_lib_dir)/valgrind/lib.*[.]a
+ 
+valgrind_image_include = 				\
+  for d in usr/bin ; do [[ -d $$d ]] && echo $$d; done ;\
+  [[ -d $(arch_lib_dir) ]]				\
+    && $(call valgrind_find_shared_libs_fn,$(arch_lib_dir))
+
