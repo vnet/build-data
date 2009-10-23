dc3d0d26bf242de21412b2ac64ebf95c2f515a01
Tim Mattox <timattox@cisco.com>
2009-10-23 14:58:20 -0700

    Setup the ompicm package so the default mca parameters are automatically set for the CRS.

--- packages/ompi.mk
+++ packages/ompi.mk
@@ -3,6 +3,7 @@
 final_prefix = /tmp/ompi
 
 ompi_platform_dir = $(call find_source_fn,$(PACKAGE_SOURCE))/contrib/platform
+ompi_platform_file= $(ompi_platform_dir)/cisco/hlfr/ebuild
 
 ompi_configure_depend = clib-install svm-install
 
@@ -20,7 +21,7 @@ ompi_configure_args_ =
 
 # combine target specific args with general configure args
 ompi_configure_args = $(ompi_configure_args_$(TARGET)) \
-  --with-platform=$(ompi_platform_dir)/cisco/hlfr/ebuild \
+  --with-platform=$(ompi_platform_file) \
   --with-crsvm=$(call package_install_dir_fn,svm) \
   --with-crsvm-libdir=$(call installed_lib_fn,svm) \
   --with-clib=$(call package_install_dir_fn,clib) \
@@ -48,5 +49,7 @@ ompi_install_args = DESTDIR='$(PACKAGE_INSTALL_DIR)'
 # that do not reflect the DESTDIR used in the install of ompi.
 # This is simpler than trying to "fix" the paths inside the .la files,
 # since we won't be using the .la files on the CRS anyway.
-ompi_post_install = rm $(PACKAGE_INSTALL_DIR)/$(final_prefix)/*/*.la
+ompi_post_install = \
+  rm $(PACKAGE_INSTALL_DIR)/$(final_prefix)/*/*.la ; \
+  cp -p $(ompi_platform_file).conf $(PACKAGE_INSTALL_DIR)/$(final_prefix)/etc/openmpi-mca-params.conf
 
