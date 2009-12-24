d9ada174cd257a5f55cb337746161b5c9e3a0857
Ralph Castain <rcastain@ubuntu.(none)>
2009-12-24 08:02:17 -0800

    Clean up the ompi make script so it can support both native and non-native pacman-based builds
    Changes reflect move to tarball-based installations - i.e., no autogen.sh

--- packages/ompi.mk
+++ packages/ompi.mk
@@ -3,27 +3,27 @@
 final_prefix = /tmp/ompi
 
 ompi_platform_dir = $(call find_source_fn,$(PACKAGE_SOURCE))/contrib/platform
-ompi_platform_file= $(ompi_platform_dir)/cisco/hlfr/ebuild
 
-ompi_configure_depend = clib-install
+ompi_configure_depend = 
 
 # target dependent configure args
 # PLATFORM=qasmp
 ompi_configure_args_ppc-q-linux = \
   --build=i386-unknown-linux-gnu
+ompi_platform_file_ppc-q-linux = $(ompi_platform_dir)/cisco/ebuild/hlfr
 
 # PLATFORM=qsp
 ompi_configure_args_ppc-qsp-linux = \
   --build=i386-unknown-linux-gnu
+ompi_platform_file_ppc-qsp-linux = $(ompi_platform_dir)/cisco/ebuild/hlfr
 
 # PLATFORM=native, NOTE: $(TARGET) is the empty string
 ompi_configure_args_ = 
+ompi_platform_file_ = $(ompi_platform_dir)/cisco/ebuild/native
 
 # combine target specific args with general configure args
 ompi_configure_args = $(ompi_configure_args_$(TARGET)) \
-  --with-platform=$(ompi_platform_file) \
-  --with-clib=$(call package_install_dir_fn,clib) \
-  --with-clib-libdir=$(call installed_lib_fn,clib)
+  --with-platform=$(ompi_platform_file_$(target))
 
 ompi_configure_prefix = --prefix=$(final_prefix)
 
@@ -42,6 +42,21 @@ ompi_configure =				\
 
 ompi_install_args = DESTDIR='$(PACKAGE_INSTALL_DIR)'
 
+ompi_build = \
+  if [ -z "$(TARGET)" ] ; then                                    \
+    if [ -L $(final_prefix) ] ; then                              \
+      rm $(final_prefix) ;                                        \
+    fi ;                                                          \
+  fi ;                                                            \
+  $(MAKE)                                                         \
+    -C $(PACKAGE_BUILD_DIR)                                       \
+    $($(PACKAGE)_make_args)                                       \
+    $(MAKE_PARALLEL_FLAGS) ;                                      \
+  if [ -z "$(TARGET)" ] ; then                                    \
+    ln -s $(PACKAGE_INSTALL_DIR)$(final_prefix) $(final_prefix) ; \
+  fi
+
+
 # Remove any installed libtool .la files, so that dependent packages
 # that use libtool don't get confused by paths embedded in the .la files
 # that do not reflect the DESTDIR used in the install of ompi.
@@ -49,5 +64,5 @@ ompi_install_args = DESTDIR='$(PACKAGE_INSTALL_DIR)'
 # since we won't be using the .la files on the CRS anyway.
 ompi_post_install = \
   rm $(PACKAGE_INSTALL_DIR)/$(final_prefix)/*/*.la ; \
-  cp -p $(ompi_platform_file).conf $(PACKAGE_INSTALL_DIR)/$(final_prefix)/etc/openmpi-mca-params.conf
+  cp -p $(ompi_platform_file_$(TARGET)).conf $(PACKAGE_INSTALL_DIR)/$(final_prefix)/etc/openmpi-mca-params.conf
 
