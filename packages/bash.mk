0048ffad24db56df0b90767728ed278eade87602
Burt Silverman <bsilverm@cisco.com>
2010-04-21 11:11:42 -0700

    Make sure glibc's getcwd() is used.
    Programs like ssh and also "telinit 1" have caused bash to be confused about
    the current working directory.
    This is because bash built in getcwd() depends upon readdir(),
    and the manner in which readdir() inode numbers differ from stat() inode
    numbers when unionfs is used.
    glibc's getcwd() has none of these issues.
    
    Also we now use bash's builtin readline library.

--- packages/bash.mk
+++ packages/bash.mk
@@ -1,14 +1,15 @@
-bash_configure_depend = readline-install ncurses-install
+bash_configure_depend =  ncurses-install
 
 bash_configure_args = --without-bash-malloc --disable-multibyte
-bash_configure_args += --with-installed-readline=$(INSTALL_DIR)/readline
 
+#this will cause getcwd to be picked up from glibc, not bash libraries.
+bash_configure_env = bash_cv_getcwd_malloc=yes
 #
 # If you need a static bash uncomment the following line
 #bash_configure_args += --enable-static-link
 
-bash_CPPFLAGS = $(call installed_includes_fn, ncurses readline)
-bash_LDFLAGS = $(call installed_libs_fn, ncurses readline)
+bash_CPPFLAGS = $(call installed_includes_fn, ncurses)
+bash_LDFLAGS = $(call installed_libs_fn, ncurses)
 
 bash_image_exclude = bin/bashbug
 bash_image_install = ln -sf /bin/bash bin/sh
