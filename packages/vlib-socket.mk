5b27a8d4ea5fa2d46fd640d4dd9c44d09366dd1a
David Barach <dbarach@ludd-vm-1.cisco.com>
2010-08-20 16:39:24 -0400

    combine vlib-api, vlib-memory, and vlib-socket

--- packages/vlib-socket.mk
+++
@@ -1,7 +0,0 @@
-vlib-socket_configure_depend = clib-install svm-install vlib-install \
-			       vlib-api-install vlib-memory-install
-
-vlib-socket_CPPFLAGS = $(call installed_includes_fn, clib svm vlib vlib-api \
-	vlib-memory)
-vlib-socket_LDFLAGS = $(call installed_libs_fn, clib svm vlib vlib-api \
-	vlib-memory)
