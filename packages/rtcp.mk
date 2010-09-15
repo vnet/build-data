4ee2c070efac1d7c92157ee61abfc86d70759ab1
Robert Burgess <roburges@ludd-vm-1.cisco.com>
2010-09-14 23:52:59 -0400

    Removed libpcap dependency for rtcp.

--- packages/rtcp.mk
+++ packages/rtcp.mk
@@ -1,11 +1,8 @@
-rtcp_build_depend = libnfnetlink-install libnetfilter_queue-install \
-			libpcap-install
+rtcp_build_depend = libnfnetlink-install libnetfilter_queue-install
 
 rtcp_CPPFLAGS = $(call installed_includes_fn, libnfnetlink)
 rtcp_CPPFLAGS += $(call installed_includes_fn, libnetfilter_queue)
-rtcp_CPPFLAGS += $(call installed_includes_fn, libpcap)
 
 rtcp_LDFLAGS = $(call installed_libs_fn, libnfnetlink)
 rtcp_LDFLAGS += $(call installed_libs_fn, libnetfilter_queue)
-rtcp_LDFLAGS += $(call installed_libs_fn, libpcap)
 
