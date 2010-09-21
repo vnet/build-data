8504cc6fd38cd9ba303d84541048e6e486657fab
Eliot Dresselhaus <edressel@sonic.net>
2010-09-21 14:54:36 -0400

    no svm dep

--- packages/vlib.mk
+++ packages/vlib.mk
@@ -1,4 +1,4 @@
-vlib_configure_depend = clib-install svm-install
+vlib_configure_depend = clib-install
 
-vlib_CPPFLAGS = $(call installed_includes_fn, clib svm)
-vlib_LDFLAGS = $(call installed_libs_fn, clib svm)
+vlib_CPPFLAGS = $(call installed_includes_fn, clib)
+vlib_LDFLAGS = $(call installed_libs_fn, clib)
