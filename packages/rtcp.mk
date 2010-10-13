8ed2fa2e086228b9fe1812d463491b61e1987389
John Denisco <jdenisco@sw-eclipse-14.cisco.com>
2010-10-13 11:07:20 -0400

    Add rtcp pacman package.

--- packages/rtcp.mk
+++ packages/rtcp.mk
@@ -1,8 +1,6 @@
-rtcp_build_depend = libnfnetlink-install libnetfilter_queue-install
+rtcp_configure_depend=libnfnetlink-install libnetfilter_queue-install
 
-rtcp_CPPFLAGS = $(call installed_includes_fn, libnfnetlink)
-rtcp_CPPFLAGS += $(call installed_includes_fn, libnetfilter_queue)
+rtcp_CPPFLAGS=$(call installed_includes_fn, libnfnetlink libnetfilter_queue)
+rtcp_LDFLAGS=$(call installed_libs_fn, libnfnetlink libnetfilter_queue)
 
-rtcp_LDFLAGS = $(call installed_libs_fn, libnfnetlink)
-rtcp_LDFLAGS += $(call installed_libs_fn, libnetfilter_queue)
-
+rtcp_image_include=  echo bin lib
