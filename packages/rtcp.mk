2fa395258b652fc352c5135c31c76f047b039b30
Rick Payne <rpayne@cisco.com>
2010-10-14 13:13:56 +0100

    Fixing...

--- packages/rtcp.mk
+++ packages/rtcp.mk
@@ -3,4 +3,4 @@ rtcp_configure_depend=libnfnetlink-install libnetfilter_queue-install
 rtcp_CPPFLAGS=$(call installed_includes_fn, libnfnetlink libnetfilter_queue)
 rtcp_LDFLAGS=$(call installed_libs_fn, libnfnetlink libnetfilter_queue)
 
-rtcp_image_include=  echo bin lib
+rtcp_image_include=  echo bin lib*
